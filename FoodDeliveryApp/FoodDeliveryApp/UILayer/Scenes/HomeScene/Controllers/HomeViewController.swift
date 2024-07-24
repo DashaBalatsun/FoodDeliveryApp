58bfc4f72f462592512ac1f6d33eb1acfdb9b9d5
DashaBalatsun <dasha.balatsun@yandex.by>
2024-07-24 21:52:39 +0300

    Added CollectionView in HomeViewController

--- FoodDeliveryApp/FoodDeliveryApp/UILayer/Scenes/HomeScene/Controllers/HomeViewController.swift
+++ FoodDeliveryApp/FoodDeliveryApp/UILayer/Scenes/HomeScene/Controllers/HomeViewController.swift
@@ -14,14 +14,35 @@ class HomeViewController: UIViewController {
         layout.scrollDirection = .horizontal
         layout.minimumLineSpacing = 40
         layout.minimumInteritemSpacing = 0
-        layout.itemSize = CGSize(width: 70, height: 91)
         let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.tag = 1
         return collection
     }()
 
+    lazy var bigHCollection: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 20
+        layout.minimumInteritemSpacing = 20
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.tag = 2
+        return collection
+    }()
+    
+    lazy var bigVCollection: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 20
+        layout.minimumInteritemSpacing = 0
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.tag = 3
+        return collection
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
+        view.backgroundColor = .systemBackground
         
     }
 }
@@ -29,14 +50,16 @@ class HomeViewController: UIViewController {
 extension HomeViewController {
     func setupLayout() {
         setupView()
-        setupmallHCollection()
+        setupSmallHCollection()
+        setupBigHCollection()
+        setupBigVCollection()
     }
     
     func setupView() {
         
     }
     
-    func setupmallHCollection() {
+    func setupSmallHCollection() {
         view.addSubview(smallHCollection)
         smallHCollection.translatesAutoresizingMaskIntoConstraints = false
         smallHCollection.backgroundColor = .red
@@ -52,22 +75,87 @@ extension HomeViewController {
         ])
     }
     
+    func setupBigHCollection() {
+        view.addSubview(bigHCollection)
+        bigHCollection.translatesAutoresizingMaskIntoConstraints = false
+        bigHCollection.backgroundColor = .red
+        bigHCollection.delegate = self
+        bigHCollection.dataSource = self
+        bigHCollection.register(BigHViewCell.self, forCellWithReuseIdentifier: "BigHViewCell")
+        
+        NSLayoutConstraint.activate([
+            bigHCollection.topAnchor.constraint(equalTo: smallHCollection.bottomAnchor, constant: 50),
+            bigHCollection.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bigHCollection.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            bigHCollection.heightAnchor.constraint(equalToConstant: 280)
+        ])
+    }
+    
+    func setupBigVCollection() {
+        view.addSubview(bigVCollection)
+        bigVCollection.translatesAutoresizingMaskIntoConstraints = false
+        bigVCollection.backgroundColor = .red
+        bigVCollection.delegate = self
+        bigVCollection.dataSource = self
+        bigVCollection.register(BigHViewCell.self, forCellWithReuseIdentifier: "BigHViewCell")
+        
+        NSLayoutConstraint.activate([
+            bigVCollection.topAnchor.constraint(equalTo: bigHCollection.bottomAnchor, constant: 50),
+            bigVCollection.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            bigVCollection.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            bigVCollection.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
 }
                 
 // MARK: - CollectionView delegate
 extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        switch collectionView.tag {
+        case 1:
+            return 30
+        case 2:
+            return 15
+        case 3:
+            return 20
+        default:
+            return 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SmallHCViewCell", for: indexPath)
-        return cell
+        switch collectionView.tag {
+        case 1:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SmallHCViewCell", for: indexPath)
+            return cell
+        case 2:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BigHViewCell", for: indexPath)
+            return cell
+        case 3:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BigHViewCell", for: indexPath)
+            return cell
+        default:
+            return UICollectionViewCell()
+        } 
     }
-    
-    
+}
+  
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        switch collectionView.tag {
+        case 1:
+            return CGSize(width: 70, height: 91)
+        case 2:
+            return CGSize(width: 130, height: 130)
+        case 3:
+            let width = collectionView.bounds.width
+            return CGSize(width: width, height: 130)
+        default:
+            return CGSize(width: 0, height: 0)
+        }
+    }
 }
                                           
                                           
-                                          
-                                          
+                                    
---
+++ FoodDeliveryApp/FoodDeliveryApp/UILayer/Scenes/HomeScene/Views/CollectionViewCell/BigVViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  BigVViewCell.swift
+//  FoodDeliveryApp
+//
+//  Created by Дарья Балацун on 24.07.24.
+//
+
+import UIKit
+
+class BigVViewCell: UICollectionViewCell {
+    
+    let topView = UIView()
+    let titleLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension BigVViewCell {
+    func setupCell() {
+        contentView.backgroundColor = .blue
+        setupTopView()
+        setupBottomLabel()
+    }
+    
+    func setupTopView() {
+        contentView.addSubview(topView)
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        topView.backgroundColor = .green
+        topView.layer.cornerRadius = 20
+        topView.layer.masksToBounds = true
+        
+        NSLayoutConstraint.activate([
+            topView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            topView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            topView.widthAnchor.constraint(equalToConstant: 130),
+            topView.heightAnchor.constraint(equalToConstant: 130)
+        ])
+         
+    }
+    
+    func setupBottomLabel() {
+        contentView.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Title label"
+        titleLabel.textColor = .white
+        titleLabel.font = .Roboto.regular.size(of: 14)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topView.topAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: 15),
+            titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)
+        ])
+    }
+}
+
+
